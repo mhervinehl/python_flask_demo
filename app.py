c7476dc341a8d674eccdc7e472740f1e69427365
mhervinehl <mathieu.hervin@ehl.ch>
2022-04-21 11:03:41 +0200

    adding link to tuto

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# tutorial from https://davemccollough.com/2020/12/05/deploy-your-first-python-flask-application-to-azure-app-service/
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
